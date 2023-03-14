62df03767484cc0b154a4cab275af937001cdf5d
Ian Mobley <ianmobley@gmail.com>
2023-03-14 15:35:50 -0700

    feat: set up eslint and prettier (#13)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+import App from './App';
+import './styles.css';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
 );
