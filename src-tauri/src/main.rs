fad8b83d278782514f9c386cf023658b0ef12695
Ian Mobley <ianmobley@gmail.com>
2023-03-15 09:40:24 -0700

    feat: add centralized logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,27 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_log::LogTarget;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
+    log::debug!("Hello from the backend!");
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .targets([
+                    LogTarget::Stdout,
+                    // LogTarget::LogDir,
+                    LogTarget::Webview,
+                ])
+                .level(log::LevelFilter::Debug)
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
