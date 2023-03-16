788a5cd12b6884a9eebad6b90ec8aaffdeb1f63f
Ian Mobley <ianmobley@gmail.com>
2023-03-16 11:33:05 -0700

    test: add escape shortcut test

--- src/components/AddFeed/AddFeed.test.tsx
+++ src/components/AddFeed/AddFeed.test.tsx
@@ -30,6 +30,30 @@ describe('AddFeed', () => {
     expect(queryByRole('heading', { name: /add a feed/i })).toBeInTheDocument();
   });
 
+  it('closes the dialog when escape is pressed', async () => {
+    const user = userEvent.setup();
+    const { getByRole, queryByRole } = render(<AddFeed />);
+
+    // form title should not be visible
+    expect(
+      queryByRole('heading', { name: /add a feed/i }),
+    ).not.toBeInTheDocument();
+
+    // open form
+    await user.click(getByRole('button', { name: /add feed/i }));
+
+    // form title should be visible
+    expect(queryByRole('heading', { name: /add a feed/i })).toBeInTheDocument();
+
+    // hit escape
+    await user.keyboard('{Escape}');
+
+    // form title should not be visible
+    expect(
+      queryByRole('heading', { name: /add a feed/i }),
+    ).not.toBeInTheDocument();
+  });
+
   it('validates the url', async () => {
     const user = userEvent.setup();
     const { getByRole, getByText } = render(<AddFeed />);
@@ -49,7 +73,7 @@ describe('AddFeed', () => {
 
   it('submits the url via ipc command', async () => {
     const user = userEvent.setup();
-    const { getByRole } = render(<AddFeed />);
+    const { getByRole, queryByRole } = render(<AddFeed />);
 
     // open form
     await user.click(getByRole('button', { name: /add feed/i }));
@@ -63,8 +87,14 @@ describe('AddFeed', () => {
     // submit
     await user.click(getByRole('button', { name: /add/i }));
 
+    // invoke was called
     expect(invoke).toHaveBeenCalledWith('add_feed', {
       url: 'https://example.com/feed.xml',
     });
+
+    // form no longer visible
+    expect(
+      queryByRole('heading', { name: /add a feed/i }),
+    ).not.toBeInTheDocument();
   });
 });
