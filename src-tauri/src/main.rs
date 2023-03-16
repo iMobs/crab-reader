5d7275c144b33ca25b22f4f92328015ceea24c71
Ian Mobley <ianmobley@gmail.com>
2023-03-16 10:30:00 -0700

    feat: add backend command for adding a feed
    todo: still needs some better error handling but that can wait.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,22 +4,37 @@
 mod rss;
 
 use anyhow::Context;
+use tauri::async_runtime::RwLock;
 use tauri_plugin_log::LogTarget;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    log::debug!("Hello from the backend!");
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn get_feeds(url_list: tauri::State<'_, FeedList>) -> Result<Vec<rss::Channel>, rss::Error> {
+    let url_list = url_list.read().await;
+    rss::get_feeds(&url_list).await
 }
 
 #[tauri::command]
-async fn get_feeds(urls: Vec<String>) -> Result<Vec<rss::Channel>, rss::Error> {
-    rss::get_feeds(&urls).await
+async fn add_feed(
+    url: String,
+    url_list: tauri::State<'_, FeedList>,
+    window: tauri::Window,
+) -> tauri::Result<()> {
+    // TODO: cache the feed and bubble up errors
+    if let Ok(_feed) = rss::get_feed(&url).await {
+        let mut url_list = url_list.write().await;
+        url_list.push(url);
+        log::debug!("url_list: {:?}", url_list);
+        window.emit("feed-refresh", url_list.clone())?;
+    }
+
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
+    let feed_list: Vec<String> = Vec::new();
+
     tauri::Builder::default()
+        .manage(RwLock::new(feed_list))
         .plugin(
             tauri_plugin_log::Builder::default()
                 .targets([
@@ -30,7 +45,9 @@ fn main() -> anyhow::Result<()> {
                 .level(log::LevelFilter::Debug)
                 .build(),
         )
-        .invoke_handler(tauri::generate_handler![greet, get_feeds])
+        .invoke_handler(tauri::generate_handler![get_feeds, add_feed])
         .run(tauri::generate_context!())
         .context("error while running tauri application")
 }
+
+type FeedList = RwLock<Vec<String>>;
