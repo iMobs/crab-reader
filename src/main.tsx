e414cf8d9236130b01e002a0a3b4fba8b5ec6269
Ian Mobley <ianmobley@gmail.com>
2023-03-18 11:37:23 -0700

    chore: switch over to sass
    Makes life a little easier for if custom styles get added

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from '~/components/App';
-import './styles.css';
+import './styles.scss';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
