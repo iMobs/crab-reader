57e4a666660a8800ef4911161102fa9d19ac6e45
Ian Mobley <ianmobley@gmail.com>
2023-03-27 10:44:23 -0700

    feat: add save/load dialogs for opml files
    Should clean up the error handling, maybe move to functions.

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -1,7 +1,16 @@
-use std::str::FromStr;
+use std::{
+    io::{BufReader, BufWriter},
+    str::FromStr,
+};
 
 use strum::{Display, EnumString};
-use tauri::{AboutMetadata, CustomMenuItem, Menu, MenuItem, Submenu, WindowMenuEvent};
+use tauri::{
+    api::dialog::FileDialogBuilder,
+    async_runtime::{block_on, RwLock},
+    AboutMetadata, CustomMenuItem, Manager, Menu, MenuItem, Submenu, WindowMenuEvent,
+};
+
+use crate::{feed, rss};
 
 #[derive(Display, EnumString)]
 #[strum(serialize_all = "snake_case")]
@@ -133,16 +142,112 @@ pub fn handle_event(event: WindowMenuEvent) {
 
         match menu_id {
             MenuId::Preferences => {}
+
             MenuId::AddSubscription => {}
+
             MenuId::NextStory => {}
+
             MenuId::PrevStory => {}
+
             MenuId::ToggleRead => {}
+
             MenuId::ToggleFavorite => {}
+
             MenuId::MarkAllRead => {}
+
             MenuId::SaveOffline => {}
+
             MenuId::ViewBrowser => {}
-            MenuId::ImportSubscriptions => {}
-            MenuId::ExportSubscriptions => {}
+
+            MenuId::ImportSubscriptions => FileDialogBuilder::new()
+                .set_title("Import Subscriptions")
+                .add_filter("OPML", &["opml"])
+                .pick_file(move |path| {
+                    let Some(path) = path else {
+                        return;
+                    };
+
+                    log::debug!("importing subscriptions from {:?}", path);
+
+                    let file = std::fs::File::open(path).expect("could not open file");
+                    let mut reader = BufReader::new(file);
+
+                    let opml = opml::OPML::from_reader(&mut reader).expect("could not parse opml");
+
+                    let app = event.window().app_handle();
+                    let manager = app.state::<RwLock<feed::Manager>>();
+                    let mut manager = manager.blocking_write();
+
+                    block_on(async {
+                        for outline in opml.body.outlines {
+                            let Some(url) = outline.xml_url else {
+                                log::warn!("no xml url");
+                                continue;
+                            };
+
+                            let channel = match rss::get_channel(&url).await {
+                                Ok(channel) => channel,
+                                Err(e) => {
+                                    log::warn!("could not fetch {}: {}", url, e);
+                                    continue;
+                                }
+                            };
+
+                            if let Err(e) = manager.ingest(&url, &channel) {
+                                log::error!("failed to ingest {}: {}", url, e);
+                            }
+                        }
+                    });
+
+                    event
+                        .window()
+                        .emit("feed-refresh", manager.subscriptions())
+                        .expect("could not emit event");
+                }),
+
+            MenuId::ExportSubscriptions => FileDialogBuilder::new()
+                .set_title("Export Subscriptions")
+                .add_filter("OPML", &["opml"])
+                .set_file_name("subscriptions.opml")
+                .save_file(move |path| {
+                    let Some(path) = path else {
+                        return;
+                    };
+
+                    log::debug!("exporting subscriptions to {:?}", path);
+
+                    let app = event.window().app_handle();
+                    let manager = app.state::<RwLock<feed::Manager>>();
+                    let manager = manager.blocking_read();
+                    let subscriptions = manager.subscriptions();
+
+                    let outlines = subscriptions
+                        .into_iter()
+                        .map(|subscription| opml::Outline {
+                            title: Some(subscription.name),
+                            xml_url: Some(subscription.url),
+                            r#type: Some("rss".to_string()),
+                            ..Default::default()
+                        })
+                        .collect();
+
+                    let title = "Crab Reader".to_string();
+                    let date_created = chrono::offset::Local::now().to_rfc2822();
+                    let head = opml::Head {
+                        title: Some(title),
+                        date_created: Some(date_created),
+                        ..Default::default()
+                    };
+                    let opml = opml::OPML {
+                        head: Some(head),
+                        body: opml::Body { outlines },
+                        ..Default::default()
+                    };
+
+                    let file = std::fs::File::create(path).expect("could not open file");
+                    let mut writer = BufWriter::new(file);
+                    opml.to_writer(&mut writer).expect("could not write opml");
+                }),
         }
     } else {
         log::debug!("unhandled menu_id: {}", menu_id);
